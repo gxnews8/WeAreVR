f40f96f6e129dbbe92a290663f83353d04841898
King <gxnews8@gmail.com>
2017-05-05 21:21:33 -0700

    WeAreVR

---
+++ components/InfoButton.js
@@ -0,0 +1,109 @@
+'use strict';
+
+import React from 'react';
+import {
+  Animated,
+  Image,
+  View,
+  VrButton,
+} from 'react-vr';
+
+import Tooltip from './Tooltip';
+
+/**
+ * On hover the InfoButton fades in a Tooltip component, and then fades it out
+ * when the cursor leaves both the button and the Tooltip.
+ */
+class InfoButton extends React.Component {
+
+  static defaultProps = {
+    fadeIn: 500,
+    fadeOut: 500,
+    height: 0.3,
+    onInput: null,
+    rotateY: 0,
+    translateZ: 0,
+    width: 0.3,
+  };
+
+  constructor(props) {
+    super();
+    this.state = {
+      hasFocus: false,
+      opacityAnim: new Animated.Value(0),
+    };
+
+  }
+
+  _fadeIn() {
+    Animated.timing(
+      this.state.opacityAnim,
+      {
+        toValue: 1,
+        duration: this.props.fadeIn,
+      }
+    ).start();
+  }
+
+  _fadeOut() {
+    Animated.timing(
+      this.state.opacityAnim,
+      {
+        toValue: 0,
+        duration: this.props.fadeOut,
+      }
+    ).start();
+  }
+
+  render() {
+
+    return (
+      <VrButton
+        style={{
+          layoutOrigin: [0.5, 0.5, 0],
+          position: 'absolute',
+          transform: [
+            {rotateY: this.props.rotateY},
+            {translateZ: this.props.translateZ},
+          ],
+        }}
+        onInput={this.props.onInput}
+        onExit={() => {
+          this._fadeOut();
+        }}
+        onClickSound={this.props.onClickSound}
+        onEnterSound={this.props.onEnterSound}
+        onExitSound={this.props.onExitSound}
+        onLongClickSound={this.props.onLongClickSound}
+      >
+        <Image
+          style={{
+            height: 0.3,
+            width: 0.3,
+            flexDirection: 'row',
+          }}
+          onEnter={() => {
+            this._fadeIn();
+          }}
+          source={this.props.source}
+        >
+        <Animated.View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            opacity: this.state.opacityAnim,
+            paddingLeft: 0.4,
+          }}
+          billboarding={'on'}
+        >
+          <Tooltip
+            tooltip={this.props.tooltip}
+          />
+        </Animated.View>
+        </Image>
+      </VrButton>
+    );
+  }
+}
+
+module.exports = InfoButton;
---
+++ index.vr.js
@@ -0,0 +1,152 @@
+/**
+ * Example ReactVR app that allows a simple app using linked 360 photos.
+ */
+'use strict';
+
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  Image,
+  Pano,
+  Text,
+  View,
+} from 'react-vr';
+
+import InfoButton from './components/InfoButton';
+import NavButton from './components/NavButton';
+import LoadingSpinner from './components/LoadingSpinner';
+
+/**
+ * ReactVR component that allows a simple app using linked 360 photos.
+ * App includes nav buttons, activated by gaze-and-fill or direct selection,
+ * that move between app locations and info buttons that display tooltips with
+ * text and/or images. Tooltip data and photo URLs are read from a JSON file.
+ */
+class WeAreVR extends React.Component {
+
+  static defaultProps = {
+    hotelSource: 'hotelData.json',
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      locationId: null,
+      nextLocationId: null,
+      rotation: null,
+    };
+
+    // Set back UI elements from the camera (which is positioned at origin).
+    this.translateZ = -3;
+  }
+
+  componentDidMount() {
+    fetch(asset(this.props.hotelSource).uri)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.init(responseData);
+      })
+      .done();
+  }
+
+  init(appConfig) {
+    // Initialize the app based on data file.
+    this.setState({
+      data: appConfig,
+      locationId: null,
+      nextLocationId: appConfig.firstPhotoId,
+      rotation: appConfig.firstPhotoRotation +
+        (appConfig.photos[appConfig.firstPhotoId].rotationOffset || 0),
+    });
+  }
+
+  render() {
+    if (!this.state.data) {
+      return null;
+    }
+
+    const locationId = this.state.locationId;
+    const photoData = (locationId && this.state.data.photos[locationId]) || null;
+    const tooltips = (photoData && photoData.tooltips) || null;
+    const rotation = this.state.data.firstPhotoRotation +
+      ((photoData && photoData.rotationOffset) || 0);
+    const isLoading = this.state.nextLocationId !== this.state.locationId;
+
+    return (
+
+      <View>
+        <View style={{transform:[{rotateY: rotation}]}}>
+          <Pano
+            // Place pano in world, and by using position absolute it does not
+            // contribute to the layout of other views.
+            style={{
+              position: 'absolute',
+              backgroundColor: isLoading ? 'rgba(0, 0, 0, 0.5)' : 'white',
+            }}
+            onLoad={() => {
+              const data = this.state.data;
+              this.setState({
+                // Now that ths new photo is loaded, update the locationId.
+                locationId: this.state.nextLocationId,
+              });
+            }}
+            source={asset(this.state.data.photos[this.state.nextLocationId].uri)}
+          />
+          {tooltips && tooltips.map((tooltip, index) => {
+            // Iterate through items related to this location, creating either
+            // info buttons, which show tooltip on hover, or nav buttons, which
+            // change the current location in the app.
+            if (tooltip.type) {
+              return(
+                <InfoButton
+                  key={index}
+                  rotateY={tooltip.rotationY}
+                  source={asset('info_icon.png')}
+                  tooltip={tooltip}
+                  translateZ={this.translateZ}
+                />
+              );
+            }
+            return(
+              <NavButton
+                key={tooltip.linkedPhotoId}
+                isLoading={isLoading}
+                onInput={() => {
+                  // Update nextLocationId, not locationId, so tooltips match
+                  // the currently visible pano; pano will update locationId
+                  // after loading the new image.
+                  this.setState({nextLocationId: tooltip.linkedPhotoId});
+                }}
+                rotateY={tooltip.rotationY}
+                source={asset(this.state.data.nav_icon)}
+                textLabel={tooltip.text}
+                translateZ={this.translateZ}
+              />
+            );
+          })}
+        </View>
+        {locationId == null &&
+          // Show a spinner while first pano is loading. Adjust layoutOrigin
+          // so it appears in center of screen. Nav Buttons also show a spinner
+          // if there is a delay is loading the next pano.
+          <View
+            style={{
+              transform: [{translateZ: this.translateZ}],
+              layoutOrigin: [0.5, 0.5, 0],
+            }}
+            height={0.5}
+            width={0.5}
+          >
+            <LoadingSpinner />
+          </View>
+        }
+      </View>
+    );
+  }
+};
+
+// Name used to create module, via reactNativeContext.createRootView('WeAreVR')
+AppRegistry.registerComponent('WeAreVR', () => WeAreVR);
+module.exports = WeAreVR;
